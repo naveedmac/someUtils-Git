41d7a7ca85ce2589157a8e9707c9e62c446af6f3
Naveed Raza <naveedraza@gmail.com>
2019-09-13 15:30:28 -0700

    Power created

--- utils.js
+++ utils.js
@@ -1,6 +1,14 @@
 function product(num1,num2){
     return num1*num2;
 }
-function product(num1,num2){
+function divide(num1,num2){
     return num1/num2;
 }
+function power(num1,num2){
+    return num1**num2
+}
+console.log(product(3, 2));
+console.log(divide(3,3));
+console.log(power(2,3));
+
+
